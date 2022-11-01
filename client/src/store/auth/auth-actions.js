b5bda6dc61b900cc95150a14ade3fbeac549c012
Dovile Brazinske <dovilebrazinske@gmail.com>
2022-11-01 22:56:54 +0200

    deleted unnecessary comments and photos

--- client/src/store/auth/auth-actions.js
+++ client/src/store/auth/auth-actions.js
@@ -10,7 +10,6 @@ import {
   AUTH_CLEAR_REDIRECT,
 } from './auth-action-types';
 
-// Paprasti action'ai
 export const authInitializedAction = { type: AUTH_INITIALIZED };
 
 export const authLoadingAction = { type: AUTH_LOADING };
@@ -21,7 +20,6 @@ export const authLogoutAction = { type: AUTH_LOGOUT };
 
 export const authClearRedirect = { type: AUTH_CLEAR_REDIRECT };
 
-// Action'ai kuriems reikia payload
 export const createAuthSuccessAction = ({ user, token, redirect }) => ({
   type: AUTH_SUCCESS,
   payload: {
@@ -36,14 +34,6 @@ export const createAuthFailureAction = (message) => ({
   payload: { message },
 });
 
-/*
- Thunk - Funkcija atliekanti salyginę ir|arba asinchroninę logiką, siųsti vieną
- ar daugiau action'ų  t.y. - vykdanti vieną ar daugiau būsenos pakitimų.
- Ši funkcija gauna tokius parametrus
-  * dispatch - funkcija siųsti action'an
-  * getState? - funkcija gauti dabartinei būsenai
-*/
-//                            ↙ function that returns thunk ↘
 export const createLoginThunkAction = (credentials, redirect) => /* return */ async (dispatch) => {
   try {
     dispatch(authLoadingAction);
@@ -57,7 +47,6 @@ export const createLoginThunkAction = (credentials, redirect) => /* return */ as
   }
 };
 
-//                            ↙ function that returns thunk ↘
 export const createAuthInitializeThunkAction = (token) => async (dispatch) => {
   try {
     dispatch(authLoadingAction);
